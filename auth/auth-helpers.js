25709293e157bd8c238d942b67a8d4d389b08c60
Tenzin Chhosphel <tenchhos@gmail.com>
2017-02-25 18:51:17 -0500

    add user profile page route, loginRequired helper method

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -29,6 +29,19 @@ function createUser(req, res) {
   });
 }
 
+function loginRequired(req, res, next) {
+  if (!req.user) return res.status(401).json({ status: 'Please log in' });
+
+  return next();
+}
+
+module.exports = {
+  comparePass,
+  loginRedirect,
+  loginRequired,
+  createUser
+}
+
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// once the user logins succesfully, render user profile page
+router.get('/', authHelpers.loginRequired, (req, res, next) => {
+  res.render('user/index', {
+    user: req.user.dataValues
+  });
+});
+
 module.exports = router;

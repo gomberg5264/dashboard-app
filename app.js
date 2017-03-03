bf7e9a95731d6e9cdf76302a3e7b4e456cd1f5f6
Tenzin Chhosphel <tenchhos@gmail.com>
2017-03-03 00:54:34 -0500

    fixing heroku file path issue

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.get('/user', (req, res) => {
 // every other route goes here
 app.get('*', (req, res) => {
   // res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-  res.sendFile('./client/build/index.html');
+  res.sendFile(__dirname + '/client/build/index.html');
 });
 
 // catch 404 and forward to error handler

6befea17a47071ec725372c173dfa5b3bdcfa192
Tenzin Chhosphel <tenchhos@gmail.com>
2017-03-04 13:11:31 -0500

    allow http stock market request

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-cross origin header issue
+// cross origin header issue
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");

9ba9ed7e9e0064dcd6fe8934a714e2df4809c624
Tenzin Chhosphel <tenchhos@gmail.com>
2017-03-09 16:19:29 -0500

    add more content to news widget

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../stylesheets/News.css';
 import axios from 'axios';
 import apiKeys from '../config.js';
+import the_next_web_logo from '../images/the_next_web_logo.png';
+import moment from 'moment';
 
 class News extends Component {
   constructor() {
@@ -10,6 +12,7 @@ class News extends Component {
       articles: []
     }
     this.showLatestNews = this.showLatestNews.bind(this);
+    this.timeConvert = this.timeConvert.bind(this);
   }
 
   componentDidMount() {
@@ -22,15 +25,25 @@ class News extends Component {
     })
   }
 
+  timeConvert (time) {
+    time = time.replace("T", ' ');
+    time = time.replace("Z", ' ');
+    var date = new Date(`${time} UTC`);
+    date = moment(date).calendar();
+    return date.toString();
+  }
+
   showLatestNews() {
     if (this.state.articles.length > 0) {
       return (
         this.state.articles.map((article) => (
           <div className="one_article">
             <a href={article.url} target="_blank">
-              <img className="pull-left" src={article.urlToImage} height="65px" width="100px"/>
+              <img className="pull-left" src={article.urlToImage} height="80px" width="100px"/>
               <h4 className="header">{article.title}</h4>
             </a>
+            <span className="pull-left">By {article.author}</span>
+            <span className="publishTime pull-right">{this.timeConvert(article.publishedAt)}</span>
             <br />
           </div>
           )
@@ -41,9 +54,13 @@ class News extends Component {
 
   render() {
     return (
-      <div className="main_News text-center">
-        <h2>Latest News
-        <span className="pull-right updated">Updated: </span>
+      <div className="main_News">
+        <h2>
+          Top Tech News
+          <span className="pull-right poweredBy">
+            Powered By:&nbsp;
+            <img src={the_next_web_logo} height="30px" width="80px" alt="logo"/>
+          </span>
         </h2>
         <hr />
         <ul>

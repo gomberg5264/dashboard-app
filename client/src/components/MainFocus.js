4be9ca0b496d05f530a0a81bb5456ded30a36637
Tenzin Chhosphel <tenchhos@gmail.com>
2017-03-12 23:32:50 -0400

    add localstorage for MainFocus component

--- client/src/components/MainFocus.js
+++ client/src/components/MainFocus.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import '../stylesheets/MainFocus.css';
 
 class MainFocus extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mainFocus: null
+    }
+  }
   render() {
+    function save(e) {
+      if (e.key === 'Enter') {
+        localStorage.setItem('main-focus', this.mainFocus.value )
+        this.mainFocus.value;
+      }
+    }
     return (
       <div>
         <div className="text-center main-focus">
@@ -10,7 +22,10 @@ class MainFocus extends Component {
           &nbsp;
           <input className="main-focus-input text-center"
             placeholder="Type Here"
+            ref={(input) => { this.mainFocus = input; }}
+            onKeyPress={save}
           />
+          {this.state.mainFocus}
         </div>
       </div>
     );

2c1bd7ac16a5aa39fb1c8bbeda387cd6143cfb11
Tenzin Chhosphel <tenchhos@gmail.com>
2017-03-12 23:27:04 -0400

    add style for MainFocus component

--- client/src/components/MainFocus.js
+++ client/src/components/MainFocus.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import '../stylesheets/MainFocus.css';
 
 class MainFocus extends Component {
   render() {
     return (
       <div>
         <div className="text-center main-focus">
-        What is your main focus for today?
-        &nbsp;
-        <input className="main-focus-input text-center"
-          placeholder="Type Here"
-        />
+          What is your main focus for today?
+          &nbsp;
+          <input className="main-focus-input text-center"
+            placeholder="Type Here"
+          />
         </div>
       </div>
     );

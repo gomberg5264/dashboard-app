ab281d7a19df859f5735fafa54bc1c291da2a6e4
Tenzin Chhosphel <tenchhos@gmail.com>
2017-03-13 22:03:07 -0400

    add stock up/down color and data custom

--- client/src/components/StockMarket.js
+++ client/src/components/StockMarket.js
@@ -22,24 +22,44 @@ class StockMarket extends Component {
   componentDidMount() {
     // stock index: s&p 500, dow, nasdaq
     var indices = ['.INX', '.DJI', '.IXIC'];
-    indices.map(index => {
+    indices.map((stockIndex, idx) => {
       $.ajax({
         type: 'GET',
         dataType: 'JSONP',
         crossDomain: true,
-        url: `http://finance.google.com/finance/info?client=ig&q=${index}`,
+        url: `http://finance.google.com/finance/info?client=ig&q=${stockIndex}`,
       })
       .done((response) => {
         const jsonData = response[0];
+        var tickerSymbol = jsonData.t;
+        // add custom name for the 3 default indices
+        if (stockIndex === '.INX') {
+          tickerSymbol = 'S&P 500';
+        }
+        if (stockIndex === '.DJI') {
+          tickerSymbol = 'DOW';
+        }
+        if (stockIndex === '.IXIC') {
+          tickerSymbol = 'NASDAQ';
+        }
         var stockData = {
+          'index': idx,
           'market': jsonData.e,
           'currentPrice': jsonData.l_fix,
-          'tickerSymbol': jsonData.t,
+          'tickerSymbol': tickerSymbol,
           'lastUpdate': jsonData.lt,
           'lastChange': jsonData.c
         };
+        // add stock class depending on last change positive or negative
+        // className used for change background to red/green
+        if (jsonData.c.charAt(0) === '+') {
+          stockData['stockClassName'] = 'stockGreen';
+        }
+        if (jsonData.c.charAt(0) === '-') {
+          stockData['stockClassName'] = 'stockRed';
+        }
         var stocks = this.state.stocks;
-        stocks.push(stockData);
+        stocks[idx] = stockData;
         this.setState({
           stocks
         })
@@ -58,7 +78,6 @@ class StockMarket extends Component {
       })
       .done((response) => {
         const jsonData = response[0];
-        console.log(jsonData);
         this.setState({
           market: jsonData.e,
           currentPrice: jsonData.l_fix,
@@ -75,8 +94,8 @@ class StockMarket extends Component {
     if (this.state.stocks) {
       return (
         this.state.stocks.map(stock => (
-            <li className="one-stock pull-left">
-              <div className="stockDesc">{stock.market}: {stock.tickerSymbol}</div>
+            <li className={"one-stock pull-left " + stock.stockClassName} >
+              <div className="stockDesc">{stock.tickerSymbol}</div>
               <div className="stockPrice">$ {stock.currentPrice}</div>
               <div>Change: {stock.lastChange}</div>
             </li>

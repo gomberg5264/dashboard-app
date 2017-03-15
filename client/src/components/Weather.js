bec8199e402227631c3f333857522885952d63a1
Tenzin Chhosphel <tenchhos@gmail.com>
2017-03-14 23:40:00 -0400

    weather forecast done

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -6,6 +6,8 @@ import apiKeys from '../config.js';
 import {Popover, OverlayTrigger} from 'react-bootstrap';
 import $ from 'jquery';
 import moment from 'moment';
+import '../stylesheets/weather-icons-wind.min.css';
+import '../stylesheets/weather-icons.min.css';
 
 class Weather extends Component {
   constructor() {
@@ -19,6 +21,7 @@ class Weather extends Component {
     this.weatherInfo = this.weatherInfo.bind(this);
     this.moreInfo = this.moreInfo.bind(this);
     this.forecast = this.forecast.bind(this);
+    this.weatherIcon = this.weatherIcon.bind(this);
   }
 
   componentDidMount() {
@@ -92,15 +95,102 @@ class Weather extends Component {
     }
   }
 
+  weatherIcon(icon) {
+    switch(icon) {
+     case("clear-day"):
+      return (
+        <i className="wi wi-day-sunny"></i>
+      )
+     break;
+
+     case("clear-night"):
+      return (
+        <i className="wi wi-night-clear"></i>
+      )
+     break;
+
+     case("rain"):
+      return (
+        <i className="wi wi-showers"></i>
+      )
+     break;
+
+     case("snow"):
+      return (
+        <i className="wi wi-snow"></i>
+      )
+     break;
+
+     case("sleet"):
+      return (
+        <i className="wi wi-night-sleet"></i>
+      )
+     break;
+
+     case("wind"):
+      return (
+        <i className="wi wi-sleet"></i>
+      )
+     break;
+
+     case("fog"):
+      return (
+        <i className="wi wi-fog"></i>
+      )
+     break;
+
+     case("cloudy"):
+      return (
+        <i className="wi wi-cloudy"></i>
+      )
+     break;
+
+     case("partly-cloudy-day"):
+      return (
+        <i className="wi wi-day-cloudy"></i>
+      )
+     break;
+
+     case("partly-cloudy-night"):
+      return (
+        <i className="wi wi-night-alt-cloudy"></i>
+      )
+     break;
+
+     case("thunderstrom"):
+      return (
+        <i className="wi wi-thunderstorm"></i>
+      )
+     break;
+
+     case("tornado"):
+      return (
+        <i className="wi wi-tornado"></i>
+      )
+     break;
+
+     case("hail"):
+      return (
+        <i className="wi wi-hail"></i>
+      )
+     break;
+
+     default:
+
+    }
+  }
+
   forecast() {
     if (this.state.weather_next_week.length > 1) {
       return (
         this.state.weather_next_week.slice(1,6).map(day => (
           <li className="one-forecast-day">
             <div>{day.time}</div>
-            <br />
-            <div className="temp-min-max-forecast">{day.temperatureMin} &#8457;/<br />
-             {day.temperatureMax} &#8457;
+              <span className="weather-icons">
+                {this.weatherIcon(day.icon)}
+              </span>
+            <div className="temp-min-max-forecast">
+              {day.temperatureMin} / {day.temperatureMax}
             </div>
           </li>
         ))
@@ -110,18 +200,16 @@ class Weather extends Component {
 
   weatherInfo() {
     if (this.state.weather_next_week.length > 0) {
-      // const weatherIcons = ['cloud', 'sun', 'clear', 'rain', 'snow', 'thunder' ];
-      // var iconClass = null;
-      // weatherIcons.map((desc, idx) => {
-      //   if (this.state.currentSummary.match(`${desc}`) !== null) {
-      //     iconClass = weatherIcons[idx];
-      //   }
-      // })
       return (
         <ul className="weather-current">
-          {/*<div className={iconClass + " iconClass"} />*/}
+          <span className="weather-icon-today">
+            {this.weatherIcon(this.state.weather_next_week[0].icon)}
+          </span>
           <h3><span className="cityName">{this.state.city}</span></h3>
           <div className="currentTemp">{this.state.currentTemp} &#8457;</div>
+          <span className="weather-icon-today">
+            {this.weatherIcon(this.state.weather_next_week[0].icon)}
+          </span>
           <div className="details">
             <li>{this.state.weather_next_week[0].summary}</li>
             <li>{this.state.weather_next_week[0].temperatureMin} &#8457;/{this.state.weather_next_week[0].temperatureMax} &#8457;</li>

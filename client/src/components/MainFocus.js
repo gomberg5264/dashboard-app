0671ea44c776e65ea3d5b8bb288e4f4fcf177de7
Tenzin Chhosphel <tenchhos@gmail.com>
2017-03-15 23:58:42 -0400

    About link add

--- client/src/components/MainFocus.js
+++ client/src/components/MainFocus.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import '../stylesheets/MainFocus.css';
 import alertify from 'alertify.js';
+import {Popover, OverlayTrigger} from 'react-bootstrap';
 
 class MainFocus extends Component {
   constructor() {
@@ -11,6 +12,7 @@ class MainFocus extends Component {
     }
     this.save = this.save.bind(this);
     this.mainFocusStatus = this.mainFocusStatus.bind(this);
+    this.moreInfo = this.moreInfo.bind(this);
   }
 
   componentDidMount() {
@@ -56,10 +58,32 @@ class MainFocus extends Component {
     }
   }
 
+  moreInfo() {
+    return (
+      <Popover id="dashboard app" title="Dashboard">
+        <p>
+          This is a <strong>dashboard app</strong> that has widgets like latest news feed, current weather report,
+          stock market data, calendar, dictionary, and a convenient collection of your daily favorite
+          apps like Facebook, Twitter, Dropbox, etc.
+        </p>
+        <p>
+          When you open your computer for the first time in the morning, this is the app you want
+          to visit because all of your basic daily needs are conveniently put together all in one
+          place - this dashboard.
+        </p>
+      </Popover>
+    );
+  }
+
   render() {
     return (
       <div>
         <div className="text-center main-focus">
+          <span className="pull-left poweredBy">
+            <OverlayTrigger placement="bottom" overlay={this.moreInfo()}>
+              <span>About</span>
+            </OverlayTrigger>
+          </span>
           What is your main focus for today?
           &nbsp;
           {this.mainFocusStatus()}
